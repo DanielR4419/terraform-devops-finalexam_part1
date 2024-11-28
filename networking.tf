6d1ce1cd431520b8f81bf80ae009e61f4dbfc7ab
DanielR4419 <147474164+DanielR4419@users.noreply.github.com>
2024-11-27 23:09:33 -0800

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -1,54 +1,77 @@
-resource "aws_instance" "jenkins" {
-  ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main_a.id
-  security_groups = [aws_security_group.web_sg.id]
-  key_name      = aws_key_pair.generated_key.key_name
-  tags = {
-    Name = "Jenkins"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "main_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "main_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "main_c" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "main_d" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "main_e" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.5.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
   }
 }
 
-resource "aws_instance" "testing" {
-  ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main_b.id
-  security_groups = [aws_security_group.web_sg.id]
-  key_name      = aws_key_pair.generated_key.key_name
-  tags = {
-    Name = "Testing"
-  }
+resource "aws_route_table_association" "main_a" {
+  subnet_id      = aws_subnet.main_a.id
+  route_table_id = aws_route_table.main.id
 }
 
-resource "aws_instance" "staging" {
-  ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main_c.id
-  security_groups = [aws_security_group.web_sg.id]
-  key_name      = aws_key_pair.generated_key.key_name
-  tags = {
-    Name = "Staging"
-  }
+resource "aws_route_table_association" "main_b" {
+  subnet_id      = aws_subnet.main_b.id
+  route_table_id = aws_route_table.main.id
 }
 
-resource "aws_instance" "production1" {
-  ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main_d.id
-  security_groups = [aws_security_group.web_sg.id]
-  key_name      = aws_key_pair.generated_key.key_name
-  tags = {
-    Name = "Production1"
-  }
+resource "aws_route_table_association" "main_c" {
+  subnet_id      = aws_subnet.main_c.id
+  route_table_id = aws_route_table.main.id
 }
 
-resource "aws_instance" "production2" {
-  ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main_e.id
-  security_groups = [aws_security_group.web_sg.id]
-  key_name      = aws_key_pair.generated_key.key_name
-  tags = {
-    Name = "Production2"
-  }
+resource "aws_route_table_association" "main_d" {
+  subnet_id      = aws_subnet.main_d.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_route_table_association" "main_e" {
+  subnet_id      = aws_subnet.main_e.id
+  route_table_id = aws_route_table.main.id
 }
