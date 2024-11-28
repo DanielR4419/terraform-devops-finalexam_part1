7b1b261fda31e785e03d539ab5c82be689679484
DanielR4419 <147474164+DanielR4419@users.noreply.github.com>
2024-11-27 23:10:02 -0800

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -1,22 +1,54 @@
-
-resource "aws_instance" "web1" {
+resource "aws_instance" "jenkins" {
   ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.main_a.id
   security_groups = [aws_security_group.web_sg.id]
   key_name      = aws_key_pair.generated_key.key_name
   tags = {
-    Name = "CCTB-ProductionEnv"
+    Name = "Jenkins"
   }
 }
 
-resource "aws_instance" "web2" {
+resource "aws_instance" "testing" {
   ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.main_b.id
   security_groups = [aws_security_group.web_sg.id]
   key_name      = aws_key_pair.generated_key.key_name
   tags = {
-    Name = "CCTB-ProductionEnv"
+    Name = "Testing"
+  }
+}
+
+resource "aws_instance" "staging" {
+  ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main_c.id
+  security_groups = [aws_security_group.web_sg.id]
+  key_name      = aws_key_pair.generated_key.key_name
+  tags = {
+    Name = "Staging"
+  }
+}
+
+resource "aws_instance" "production1" {
+  ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main_d.id
+  security_groups = [aws_security_group.web_sg.id]
+  key_name      = aws_key_pair.generated_key.key_name
+  tags = {
+    Name = "Production1"
+  }
+}
+
+resource "aws_instance" "production2" {
+  ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main_e.id
+  security_groups = [aws_security_group.web_sg.id]
+  key_name      = aws_key_pair.generated_key.key_name
+  tags = {
+    Name = "Production2"
   }
 }
