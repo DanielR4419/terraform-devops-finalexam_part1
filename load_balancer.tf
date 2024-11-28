5e7bbbc7089a79b35e43c38cca977a2b41102477
DanielR4419 <147474164+DanielR4419@users.noreply.github.com>
2024-11-27 23:11:20 -0800

    Update load_balancer.tf

--- load_balancer.tf
+++ load_balancer.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "web_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.web_sg.id]
-  subnets            = [aws_subnet.main_a.id, aws_subnet.main_b.id]
+  subnets            = [aws_subnet.main_d.id, aws_subnet.main_e.id]
 }
 
 resource "aws_lb_target_group" "web_tg" {
@@ -33,14 +33,14 @@ resource "aws_lb_listener" "web_lb_listener" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "web1" {
+resource "aws_lb_target_group_attachment" "production1" {
   target_group_arn = aws_lb_target_group.web_tg.arn
-  target_id        = aws_instance.web1.id
+  target_id        = aws_instance.production1.id
   port             = 80
 }
 
-resource "aws_lb_target_group_attachment" "web2" {
+resource "aws_lb_target_group_attachment" "production2" {
   target_group_arn = aws_lb_target_group.web_tg.arn
-  target_id        = aws_instance.web2.id
+  target_id        = aws_instance.production2.id
   port             = 80
 }
